1b96bd72559fb28126c2ef5edd9eb72f64ee0d88
kirk <kirk@apple.local>
2022-10-16 16:43:32 -0400

    util

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-
 import lipser
-# from lipser.util import math
-from lipser.util import array
-from lipser.util import iter
-from lipser.topology import complex
-# from lipser import data
-# from lipser import geometry
-# from lipser import graph
-# from lipser import plot
-# from lipser import topology
-# import lipser.util.array as array
 
-import numpy as np
+from lipser.util import array, iter
 
-G = np.eye(4)
-for t in array.get_grid(4):
-    print(t)
+print(f"\nutil.array\n\t{array.get_grid(2)}")
+print(f"\nutil.iter\n\t{iter.lmap(lambda x: 2*x, 'misisipi')}\n")
 
-print(iter.lmap(lambda x: 2*x, 'misisipi'))
+
+# from lipser.topology import complex, persistence
+# from lipser.geometry import util
