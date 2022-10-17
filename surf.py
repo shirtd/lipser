05a3d570b5285337b81d9c372b8163f63886e456
kirk <kirk@apple.local>
2022-10-16 20:06:38 -0400

    surf

--- surf.py
+++ surf.py
@@ -5,6 +5,11 @@ from contours.data import make_grid
 from lipser.util.math import mk_gauss
 
 
+SAVE = True
+DIR = 'data'
+NAME = 'surf'
+EXT = 'csv'
+
 RES = 32
 SHAPE = (2,1)
 GAUSS_ARGS = [  (1,     [-0.2, 0.2],    [0.3, 0.3]),
@@ -13,11 +18,6 @@ GAUSS_ARGS = [  (1,     [-0.2, 0.2],    [0.3, 0.3]),
                 (0.8,   [-0.8, -0],     [0.4, 0.4]),
                 (0.4,   [0.6, 0.0],     [0.4, 0.2]),
                 (0.7,   [1.25, 0.3],    [0.25, 0.25])]
-
-SAVE = True
-DIR = 'data'
-NAME = 'surf'
-EXT = 'csv'
 
 
 if __name__ == '__main__':
