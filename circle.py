9418a917e6822dcb48caaa3716315f05d170c14e
kirk <kirk@apple.local>
2022-10-17 23:58:54 -0400

    madness

--- circle.py
+++ circle.py
@@ -46,12 +46,22 @@ parser = argparse.ArgumentParser(prog='circler')
 parser.add_argument('--dir', default=os.path.join('figures','lips'), help='dir')
 parser.add_argument('--file', default='data/surf_279_2e-1.csv', help='file')
 
+
+# def plot_points(self, points, key, radius=DEFAULT['point']['radius'], **kwargs):
+#     points = np.array([p for p in points if all(0 <= c <= l for l,c in zip(self.bounds, p))])
+#     element = pyvista.PolyData(points).glyph(scale=False, geom=pyvista.Sphere(radius=radius))
+#     kwargs = {**{'color' : DEFAULT['point']['color']}, **kwargs}
+#     return self.add_element(element, key, **kwargs)
+
 if __name__ == '__main__':
 
     SKIP = 4
-    FRAMES = 50
+    FRAMES = 100
     COEF = 60
     SCALE = None
+    WEIGHT = 10
+
+    LENGTH = 30
 
     for i in range(len(mics)):
         try:
@@ -62,29 +72,46 @@ if __name__ == '__main__':
     kw = {  'in':   {   'samplerate':   148000},
             'out':  {   'samplerate':   148000}}
 
+    plt.ion()
+    # plt.scatter(data[:,0]+1, data[:,1]+1, s=1, alpha=0.1)
+    # plt.scatter(data[:,2], data[:,3], s=2, alpha=0.1)
+    fig, ax = plt.subplots()
+    ax.axis('off')
+
+    if SCALE is not None:
+        ax.set_xlim(-SCALE,SCALE)
+        ax.set_ylim(-SCALE,SCALE)
+
     with (  default_mic.recorder(**kw['in']) as mic ) : # ,
             # default_speaker.player(**kw['out']) as sp):
-        print("Recording...")
-        _data = mic.record(numframes=COEF*FRAMES)
-        data = _data[::SKIP]
+
+
+        for i in range(LENGTH):
+            print("Recording...")
+            _data = mic.record(numframes=COEF*FRAMES)
+
+            data = _data[::SKIP]*i/100
+            time = np.linspace(0, WEIGHT, len(data))
+            ax.plot(data[:,4]*time, data[:,5]*time, alpha=0.25,lw=0.5)
+            plt.pause(1.1)
+
 
     # fout = 'data/drip2.txt'
     # print("saving %s" % fout)
     # np.savetxt(fout, data)
 
-    plt.ion()
-    # plt.scatter(data[:,0]+1, data[:,1]+1, s=1, alpha=0.1)
-    # plt.scatter(data[:,2], data[:,3], s=2, alpha=0.1)
-    fig, ax = plt.subplots()
-    ax.plot(data[:,4], data[:,5], alpha=0.15)
-    ax.axis('off')
-    if SCALE is not None:
-        ax.set_xlim(-SCALE,SCALE)
-        ax.set_ylim(-SCALE,SCALE)
-    # plt.show()
 
-    if input('save? ') in {'y','yes','Y'}:
-        plt.savefig('data/last_frame.png')
+    # import pyvistaqt as pvqt
+    # import pyvista
+    # pvqt.BackgroundPlotter()
+    # element = pyvista.PolyData(np.vstack([data[:,4:].T,np.array(range(len(data)))]).T).glyph(scale=False, geom=pyvista.Sphere(radius=10))
+
+
+
+
+    # if input('save? ') in {'y','yes','Y'}:
+    #     plt.savefig('data/last_frame.png')
+
 
     # surf = ScalarFieldData(SURF_PATH, make_grid(RES, SHAPE))
     # sample = SampleData(SAMP_PATH)
